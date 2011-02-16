04cacdabd5fd66e8ae8134bedd6241319b19d1bd
Simon Bächler <sb@feinheit.ch>
2011-02-16 11:03:26 +0100

    fixed agenda to work with the new FeinCSM

--- models.py
+++ models.py
@@ -4,15 +4,16 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from feinheit import translations
-from feincms.views.generic.list_detail import object_list
+#from feincms.views.generic.list_detail import object_list
 from django.core.paginator import Paginator
 from django.template.loader import render_to_string
+from django.template.context import RequestContext
 
 
 
 class Event(models.Model, translations.TranslatedObjectMixin):
     date = models.DateField(_('date'), default=date.today)
-    time = models.CharField(_('time'), max_length=50, blank=True)
+    time = models.CharField(_('time'), max_length=15, blank=True)
     image = models.ImageField(_('image'), upload_to='event_images',
         blank=True, null=True)
 
@@ -49,11 +50,14 @@ class EventsContent(models.Model):
         verbose_name = _('event list')
         verbose_name_plural = _('event lists')
 
-    def render(self, request):
+    def render(self, **kwargs):
+        request = kwargs.get('request')
         if self.which == 'u':
             object_list = Event.objects.filter(date__gte=date.today)
         else:
             object_list = Event.objects.filter(date__lte=date.today)
         current_page = request.GET.get('page', 1)  
         page = Paginator(object_list, 20).page(current_page)
-        return render_to_string('agenda/event_list.html', locals())
+        return render_to_string('content/agenda/event_list.html', 
+                {'object_list': object_list, 'page': page, },
+                context_instance=RequestContext(request))
