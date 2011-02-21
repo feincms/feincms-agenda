ce1baa1c7936a12b1c545bad0a074467370d6987
Simon Bächler <sb@feinheit.ch>
2011-02-21 17:19:29 +0100

    agenda richtext cleanse methode added. New template

--- models.py
+++ models.py
@@ -4,14 +4,29 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from feinheit import translations
+from django.conf import settings
 #from feincms.views.generic.list_detail import object_list
 from django.core.paginator import Paginator
 from django.template.loader import render_to_string
 from django.template.context import RequestContext
-
+from django.core.exceptions import ImproperlyConfigured
+from feincms.utils.html import cleanse
 
 
 class Event(models.Model, translations.TranslatedObjectMixin):
+    
+    def __init__(self, *args, **kwargs):
+        super(Event, self).__init__(*args, **kwargs)
+        self.cleanse = getattr(settings, 'EVENT_CLEANSE', False)
+        cm = getattr(settings, 'CLEANSE_MODULE', None)
+        if cm:
+            try:
+                self.cleanse_module = __import__(cm, fromlist=True)
+            except (ValueError, ImportError):
+                raise ImproperlyConfigured, 'There was an error importing your %s cleanse_module!' % self.__name__        
+        else:
+            self.cleanse_module = cleanse
+        
     date = models.DateField(_('date'), default=date.today)
     time = models.CharField(_('time'), max_length=15, blank=True)
     image = models.ImageField(_('image'), upload_to='event_images',
@@ -23,7 +38,7 @@ class Event(models.Model, translations.TranslatedObjectMixin):
         verbose_name_plural = _('events')
 
     objects = translations.TranslatedObjectManager()
-
+    
 
 class EventTranslation(translations.Translation(Event)):
     title = models.CharField(_('title'), max_length=100)
@@ -36,6 +51,12 @@ class EventTranslation(translations.Translation(Event)):
 
     def __unicode__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        # TODO: Move this to the form?
+        if getattr(self.parent, 'cleanse', False):
+            self.description = self.parent.cleanse_module.cleanse_html(self.description)
+        super(EventTranslation, self).save(*args, **kwargs)
 
     @models.permalink
     def get_absolute_url(self):
