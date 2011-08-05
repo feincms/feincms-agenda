5c1d4cfd6f39e556ad344fc91ab03216e3d97f5a
Simon Schmid <ssc@feinheit.ch>
2011-08-05 20:18:41 +0200

    new params for agenda api

--- api.py
+++ api.py
@@ -8,8 +8,12 @@ from models import Event
 @standalone
 def events(request):
     filter = request.REQUEST.get('filter', 'upcoming')
+    limit = request.REQUEST.get('limit', 0)
+    id = request.REQUEST.get('id', 0)
     
-    if filter == 'past':
+    if id:
+        events = Event.objects.active().filter(id=id)
+    elif filter == 'past':
         events = Event.objects.past()
     elif filter == 'active':
         events = Event.objects.active()
@@ -17,5 +21,8 @@ def events(request):
         events = Event.objects.all()
     else:
         events = Event.objects.upcoming()
+        
+    if limit:
+        events = events[:limit]
         
     return HttpResponse(serializers.serialize('json', events, ensure_ascii=False), mimetype="text/javascript")
